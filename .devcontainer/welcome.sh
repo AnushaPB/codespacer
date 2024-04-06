79565058c7b20b8e6d3cbf81415c0143c68d9bed
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2024-04-06 10:37:28 -0700

    Create welcome.sh

---
+++ .devcontainer/welcome.sh
@@ -0,0 +1,17 @@
+# Modified from: https://github.com/boettiger-lab/nasa-topst-env-justice/blob/main/.devcontainer/setup.sh
+mkdir -p ~/.local/share/rstudio/projects_settings
+export RPROJ"=$(ls ${CODESPACE_VSCODE_FOLDER}/*.Rproj)"
+echo ${RPROJ} > ~/.local/share/rstudio/projects_settings/last-project-path
+
+
+# Construct the message
+message="## [Open in RStudio](https://$CODESPACE_NAME-8787.app.github.dev)
+"
+# Echo the message to the terminal
+echo "
+👋 Welcome to Codespaces! You are on our custom image. 
+   - It includes runtimes and tools for R using RStudio 
+
+🌐 Open the RStudio editor here: https://$CODESPACE_NAME-8787.app.github.dev
+   - (This may take a few seconds to load, retry if necessary)
+"
