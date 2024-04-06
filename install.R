d4481967e597fb72c14bd3265207bc5cbf76054e
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2024-04-06 10:27:05 -0700

    Create install.R

---
+++ install.R
@@ -0,0 +1,7 @@
+#! /usr/local/bin/Rscript
+# Install R dependencies
+
+# Add lines here for installing packages:
+# For example:
+# install.packages("wingen")
+# remotes::install_github('AnushaPB/wingen')
