4de740ce623246aac8cd4496d3df0e3dfbf32163
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2024-05-25 09:33:00 -0700

    Remove RStudio from welcome.sh

--- .devcontainer/welcome.sh
+++ .devcontainer/welcome.sh
@@ -10,16 +10,3 @@ export RPROJ"=$(ls ${CODESPACE_VSCODE_FOLDER}/*.Rproj)"
 
 # Write the path of the .Rproj file to the last-project-path file in the RStudio project settings directory
 echo ${RPROJ} > ~/.local/share/rstudio/projects_settings/last-project-path
-
-# Construct the welcome message ----------------------------------------------------------------------------
-
-message="## [Open in RStudio](https://$CODESPACE_NAME-8787.app.github.dev)
-"
-# Echo the message to the terminal
-echo "
-👋 Welcome to Codespaces! You are on our custom image. 
-   - It includes runtimes and tools for R using RStudio 
-
-🌐 Open the RStudio editor here: https://$CODESPACE_NAME-8787.app.github.dev
-   - (This may take a few seconds to load, retry if necessary)
-"
